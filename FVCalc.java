09242cd564277d1ecf0e5f9f323691836068e72c
RotemKulka <rotem.kulka@post.runi.ac.il>
2025-11-03 23:41:48 +0200

    Update FVCalc.java

--- FVCalc.java
+++ FVCalc.java
@@ -5,6 +5,6 @@ public class FVCalc {
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
 		double futureValue = currentValue*(Math.pow(1+(rate/100),(double)n));
-		System.out.println("After "+n+" yaers, $"+currentValue+" saved at "+rate+"% will yield $"+(int)futureValue);
+		System.out.println("After "+n+" years, $"+currentValue+" saved at "+rate+"% will yield $"+(int)futureValue);
 	}
 }
