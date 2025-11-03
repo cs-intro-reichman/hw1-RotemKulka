2b0c607f891d9a22c91f03746e84a845d25394eb
RotemKulka <rotem.kulka@post.runi.ac.il>
2025-11-03 23:01:16 +0200

    Update NumWords.java

--- NumWords.java
+++ NumWords.java
@@ -2,9 +2,9 @@
 public class NumWords {
 	public static void main(String args[]) {
 		int num = Integer.parseInt(args[0]);
-		int hundreds = (num%1000)/100; //It can only be divided by 100, but in order to cover cases where the input is greater than 3 digits, we will use its module earlier.
+		int hundreds = num/100;
 		int tens = (num/10)%10;
 		int ones = num%10;
 		System.out.println(hundreds+" hundreds, "+tens+" tens, and "+ones+" ones.");
-}
+    }
 }
